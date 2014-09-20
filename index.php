b93ba8d94658deb2d7208b1ea6d3dca8c3ae5ce9
Thulio Philipe <thulioph@gmail.com>
2014-09-20 00:16:17 -0300

    Adds a new structure HTML

--- index.php
+++ index.php
@@ -1,50 +1,45 @@
-<?php include("inc/header.php") ?>
+<?php include('inc/head.php') ?>
 
-	<div class="off-canvas-wrap" data-offcanvas>
+<header class="header-primary">
+  <button class="btn btn-primary">Quero Pedalar</button>
 
-	  <div class="inner-wrap">
+  <ul class="list-actions">
+    <li class="list-item">
+      <a href="#" class="list-link" id="alone" title="Quero pedalar sozinho">Sozinho</a>
+    </li>
 
-	    <!-- nav -->
-	    <nav class="tab-bar">
-        <!-- btn left canvas -->
-	      <section class="left-small">
-	        <a class="left-off-canvas-toggle menu-icon" href="#">
-	        	<span></span>
-	        </a>
-	      </section>
-
-	      <section class="middle tab-bar-section">
-	        <h1 class="title-primary">Quero Pedalar</h1>
-	      </section>
-      </nav>
-
-      <!-- btn right canvas -->
-      <section class="right-small">
-        <a class="menu-icon" href="#"></a>
-      </section>
+    <li class="list-item">
+      <a href="#" class="list-link" id="group" title="Quero pedalar em grupo">Em grupo</a>
+    </li>
+  </ul>
+</header>
 
 
-	    <!-- menu esquerdo -->
-	    <aside class="left-off-canvas-menu">
-	      <ul class="off-canvas-list">
-	        <li><label class="canvas-title">Quero Pedalar</label></li>
-	        <li class="canvas-list" id="alone">Sozinho</li>
-	        <li class="canvas-list" id="group">Em Grupo</li>
-	      </ul>
+<main id="main" class="container-fluid main">
+  <div class="row">
+    <h2 class="col-xs-12 subtitle">Sozinho</h2>
 
-	     	<?php include('inc/legends.php') ?>
-	    </aside>
+    <ul class="col-xs-10 col-xs-offset-1 list-alone" id="list-alone">
+      <li class="item-alone">
+        <h3 class="title-local">Praça da Jaqueira</h3>
+        <small class="distance">1,5km de distância</small>
 
-	    <section id="main-section" class="main-section">
-	    </section>
-
-	  	<a class="exit-off-canvas"></a>
-
-	  </div>
-
-	 <div id="map"></div>
-
+        <figure class="icon">
+          <img src="http://dummyimage.com/40x40/4d494d/686a82.gif&text=icon" alt="placeholder+image">
+        </figure>
+      </li>
+    </ul>
   </div>
+</main>
 
 
-<?php include("inc/footer.php") ?>
+<aside class="map" id="map"></aside> <!-- Map -->
+
+
+<footer class="footer-primary">
+  <div class="container-fluid">
+    <div class="row"></div>
+  </div>
+</footer>
+
+<?php include('inc/scripts.php') ?>
