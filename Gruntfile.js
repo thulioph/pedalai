3e61908e7d12a63f5e91bb41acff4d3707136bee
Thulio Philipe <thulioph@gmail.com>
2014-09-20 23:47:00 -0300

    Edits tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,9 @@
 "use strict";
 
 module.exports = function( grunt ) {
-	// Definição dos arquivos js
-	var filesJS = [
+
+  // Definição dos arquivos js
+  var filesJS = [
             'src/js/APP.js',
             'src/js/APP.Geolocation.js',
             'src/js/APP.Home.js',
@@ -10,116 +11,116 @@ module.exports = function( grunt ) {
             'src/js/APP.List.js'
         ];
 
-	// Load all tasks
-	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  // Load all tasks
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-	grunt.initConfig({
-		// Watch
-		watch: {
-			css: {
-				files: [ 'src/sass/**/*' ],
-				tasks: [ 'compass:src', 'concat:css' ]
-			},
+  grunt.initConfig({
+    // Watch
+    watch: {
+      css: {
+        files: [ 'src/sass/**/*' ],
+        tasks: 'compass'
+        // tasks: [ 'compass:dist', 'concat:css' ]
+      },
 
-			js: {
-				files: 'src/js/**/*',
-				tasks: [ 'concat:js' ]
-			}
-		},
+      js: {
+        files: 'src/js/**/*',
+        tasks: [ 'concat:js' ]
+      }
+    },
 
-		// Compass scss
-		compass: {
-			src: {
-				options: {
-					config: 'config.rb'
-				}
-			},
-
-			dist: {
-				options: {
-					force: true,
-					config: 'config.rb',
-					outputStyle: 'compressed',
-					relativeAssets: true
-				}
-			}
-		},
-
-		// Concateção dos arquivos CSS e JS
-		concat: {
-			css: {
-				src: 'src/css/main.css',
-				dest: 'dist/css/styles.combined.min.css'
-			},
-
-			js: {
-				src: filesJS,
-
-				dest: 'dist/js/scripts.combined.min.js'
-			}
-		},
-
-		// Concateção e minificação dos arquivos e JS
-		uglify: {
-			options: {
-				mangle: false
-			},
-
-			dist: {
-				files: {
-					'dist/js/scripts.combined.min.js': filesJS
-				}
-			}
-		},
-
-		// Otimização das imagens
-		imagemin: {
-		    dynamic: {
-		    	files: [{
-		        	expand: true,
-		        	cwd: 'src/images',
-		        	src: ['**/*.{png,jpg,gif}'],
-		        	dest: 'dist/images'
-		    	}]
-		    }
-		},
-
-		browserSync: {
-            files: {
-
-                // Aplicando o recurso de Live Reload nos seguintes arquivos
-                src : [
-                	'dist/css/styles.combined.min.css',
-                	'dist/js/scripts.combined.min.js',
-                	'**/*.html',
-                	'**/*.php'
-                ]
-
-            },
-
-            options: {
-
-                // Definindo um IP manualmente
-                host : "177.95.199.42",
-
-                // Integrando com a tarefa "watch"
-                watchTask: true,
-
-                // Sincronizando os eventos entre os dispositívos
-                ghostMode: {
-                	clicks: true,
-                    scroll: true,
-                    links: true,
-                    forms: true
-                }
-            }
+    // Compile scss
+    compass: {
+      src: {
+        options: {
+          sassDir: 'src/sass',
+          cssDir: 'src/css',
+          environment: 'production'
         }
-	});
+      },
 
-	// registrando tarefa default
-	// grunt.registerTask( 'default', [ 'browserSync', 'watch' ] );
+      dist: {
+        options: {
+          force: true,
+          config: 'config.rb'
+        }
+      }
+    },
+
+    // Concat css
+    concat: {
+      css: {
+        src: 'src/css/main.css',
+        dest: 'dist/css/styles.combined.min.css'
+      },
+
+      js: {
+        src: filesJS,
+        dest: 'dist/js/scripts.combined.min.js'
+      }
+    },
+
+    // Concat and minify javascripts
+    uglify: {
+      options: {
+        mangle: false
+      },
+
+      dist: {
+        files: {
+          'dist/js/scripts.combined.min.js' : filesJS
+        }
+      }
+    },
+
+    // Optimizes images
+    imagemin: {
+        dynamic: {
+          files: [{
+              expand: true,
+              cwd: 'src/images',
+              src: ['**/*.{png,jpg,gif}'],
+              dest: 'dist/images'
+          }]
+        }
+    },
+
+    // Clean old images
+    clean: {
+      build: {
+        src: ['dist/images']
+      }
+    },
+
+    // Browser Sync
+    browserSync: {
+      files: {
+        src: [
+          'dist/css/styles.combined.min.css',
+          'dist/js/scripts.combined.min.js',
+          '**/*.html',
+          '**/*.php'
+        ]
+      },
+
+      options: {
+        host: '177.95.199.42', // Definindo um IP manualmente
+        watchTask: true, // Integração com a taks watch
+        ghostMode: { //Sincronizando os eventos entre os dispositivos
+          clicks: true,
+          scroll: true,
+          links: true,
+          forms: true
+        }
+      }
+    }
+  });
+
+  // registrando tarefa default
+  // grunt.registerTask( 'default', [ 'browserSync', 'watch' ] );
   grunt.registerTask( 'default', [ 'watch' ] );
-	grunt.registerTask( 'img', [ 'imagemin' ] );
-	grunt.registerTask( 'src', [ 'compass:src', 'concat:js', 'concat:css' ] );
-	grunt.registerTask( 'dist', [ 'compass:dist', 'uglify:dist', 'concat:css', 'imagemin' ] );
+  grunt.registerTask( 'img', [ 'clean', 'imagemin' ] );
+
+  grunt.registerTask( 'src', [ 'watch', 'compass:src', 'concat:js', 'concat:css' ] );
+  grunt.registerTask( 'dist', [ 'compass:dist', 'uglify:dist', 'concat:css', 'imagemin' ] );
 };
